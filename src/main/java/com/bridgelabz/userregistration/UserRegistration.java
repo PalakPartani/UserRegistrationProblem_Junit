22d68c388df489785b064e9657501e49fdc4fbe5
PalakPartani <partanipalak16@gmail.com>
2020-04-20 01:15:57 -0700

    ADDED:feature to validate lastname

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -2,7 +2,7 @@ package com.bridgelabz.userregistration;
 
 public class UserRegistration {
 
-    static String USER_NAME ="^[A-Z][a-z]{2,}$";
+    static String USER_NAME = "^[A-Z]{1}[a-z]{2,}$";
 
     public static boolean validInputName(String name) {
         return name.matches(USER_NAME);
